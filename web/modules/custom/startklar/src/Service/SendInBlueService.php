c92a021c4e99a604103c5e789bce47c1e2e19528
Max Bachhuber <max.bachhuber@bahuma.io>
2022-08-24 00:20:50 +0200

    Add debug mode for SendInBlueService

--- web/modules/custom/startklar/src/Service/SendInBlueService.php
+++ web/modules/custom/startklar/src/Service/SendInBlueService.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\startklar\Service;
 
+use Drupal\Core\Http\RequestStack;
 use Drupal\Core\Url;
 use GuzzleHttp\Client;
 use SendinBlue\Client\Api\ContactsApi;
@@ -31,7 +32,9 @@ class SendInBlueService {
 
   protected int $TEILNEHMER_LIST_ID;
 
-  public function __construct() {
+  protected bool $isDebugMode;
+
+  public function __construct(RequestStack $requestStack) {
     $this->API_KEY = getenv('SEND_IN_BLUE_API_KEY');
     $this->NEWSLETTER_LIST_ID = intval(getenv('SEND_IN_BLUE_NEWSLETTER_LIST_ID'));
     $this->DOI_TEMPLATE_ID = intval(getenv('SEND_IN_BLUE_DOUBLE_OPT_IN_TEMPLATE_ID'));
@@ -39,6 +42,7 @@ class SendInBlueService {
     $this->FRONTEND_URL_ANMELDUNG = getenv('FRONTEND_URL_ANMELDUNG');
     $this->GRUPPENANMELDUNG_TEMPLATE_ID = getenv('SEND_IN_BLUE_GRUPPENANMELDUNG_TEMPLATE_ID');
     $this->TEILNEHMER_LIST_ID = intval(getenv('SEND_IN_BLUE_TEILNEHMER_LIST_ID'));
+    $this->isDebugMode = str_starts_with($requestStack->getMainRequest()->getHttpHost(), 'localhost');
   }
 
   protected function getApiClient() {
@@ -60,7 +64,13 @@ class SendInBlueService {
     $createDoiContact->setIncludeListIds([$this->NEWSLETTER_LIST_ID]);
     $createDoiContact->setRedirectionUrl(Url::fromUri($this->FRONTEND_URL, ['query' => ['emailConfirmed' => TRUE]])->setAbsolute(TRUE)->toString());
 
-    $apiClient->createDoiContact($createDoiContact);
+    if ($this->isDebugMode) {
+      \Drupal::logger('startklar_sendinblue')->info('Send Double Opt In Mail. ' . print_r([
+        'createDoiContact' => $createDoiContact,
+      ], TRUE));
+    } else {
+      $apiClient->createDoiContact($createDoiContact);
+    }
   }
 
   /**
@@ -90,15 +100,19 @@ class SendInBlueService {
       'STARTKLAR_GRUPPENNUMMER' => $groupId,
     ];
 
-    $apiInstance->sendTransacEmail($sendSmtpEmail);
+    if ($this->isDebugMode) {
+      \Drupal::logger('startklar_sendinblue')->info('Send Gruppenanmeldung Email ' . print_r([
+          '$sendSmtpEmail' => $sendSmtpEmail,
+        ], TRUE));
+    } else {
+      $apiInstance->sendTransacEmail($sendSmtpEmail);
+    }
   }
 
   /**
    * @throws \SendinBlue\Client\ApiException
    */
   public function addTeilnehmer($mail) {
-    error_reporting(E_ALL);
-    ini_set('display_errors', 1);
     $apiClient = $this->getApiClient();
 
     $contact = new CreateContact();
@@ -106,7 +120,13 @@ class SendInBlueService {
     $contact->setListIds([$this->TEILNEHMER_LIST_ID]);
 
     try {
-      $apiClient->createContact($contact);
+      if ($this->isDebugMode) {
+        \Drupal::logger('startklar_sendinblue')->info('Add teilnehmer to list. ' . print_r([
+            '$contact' => $contact,
+          ], TRUE));
+      } else {
+        $apiClient->createContact($contact);
+      }
     } catch (ApiException $e) {
       $body = json_decode($e->getResponseBody());
 
@@ -132,9 +152,18 @@ class SendInBlueService {
     $apiClient = $this->getApiClient();
 
     try {
-      $apiClient->removeContactFromList($this->TEILNEHMER_LIST_ID, new RemoveContactFromList([
+      $data = new RemoveContactFromList([
         'emails' => [$mail],
-      ]));
+      ]);
+
+      if ($this->isDebugMode) {
+        \Drupal::logger('startklar_sendinblue')->info('Remove teilnehmer from list. ' . print_r([
+            '$data' => $data,
+          ], TRUE));
+      } else {
+        $apiClient->removeContactFromList($this->TEILNEHMER_LIST_ID, $data);
+      }
+
     } catch (ApiException $e) {
       // If it cannot be removed, it was not in the list.
     }
